122f50cbc14892479c6e8505a289b58c08a7291c
Saurabh Kumar <138187379+Saurabhkumar0421@users.noreply.github.com>
2023-09-04 09:49:41 +0530

    Add files via upload

---
+++ sec_upgr.py
@@ -0,0 +1,8 @@
+def calculate_area(side_length):
+    return side_length ** 2
+
+side_length = float(input("Enter the side length of the square: "))
+
+area = calculate_area(side_length)
+
+print(f"The area of the square with side length {side_length} is: {area}")
---
+++ upgrade.py
@@ -0,0 +1,16 @@
+a = float(input("Enter the first number (a): "))
+b = float(input("Enter the second number (b): "))
+
+#Floor Division
+floor_division_result = a // b
+
+#a power b
+power_result = a ** b
+
+#Check if a and b are equal
+are_equal = a == b
+
+# Display the results
+print(f"Floor Division of {a} by {b} is: {floor_division_result}")
+print(f"{a} raised to the power of {b} is: {power_result}")
+print(f"Are {a} and {b} equal? {are_equal}")
